7b8b436dfd03b7682459772f0975dee9965b3752
Antu <122485403+antukung@users.noreply.github.com>
2023-05-22 19:28:51 -0300

    aca esta

--- PARTE 2/algoritmos.js
+++ PARTE 2/algoritmos.js
@@ -10,34 +10,32 @@ function clickIngreso() {
 contadorClick= ++contadorClick;
     switch (contadorClick) {
         case 1:
-    divTipo.innerHTML=`
-        <legend>Dias</legend>
-        <select name="" id="select-dias">
+    divTipo.innerHTML=`<legend>Dias</legend>`
+    selectCamas.innerHTML=<select name="Dias" id="select-dias">
             <option value="1">1</option>
             <option value="3">3</option>
             <option value="7">7</option>
-        </select>`
+        </select>
     
             
             break;
         case 2:
-            divTipo.innerHTML=`<legend> Ingresan</legend>
-            <select name="" id="select-ingreso">
+            divTipo.innerHTML=`<legend> Ingresan</legend>}`
+            selectCamas.innerHTML=<select name="Ingresan" id="select-ingreso">
                 <option value="Lunes">Lunes</option>
                 <option value="Miercoles">Miercoles</option>
                 <option value="Sabado">Sabado</option>
             </select>
                
-                `
                         
             break;
         case 3:
-        divTipo.innerHTML=`<legend> Sevicios extra</legend>
-        <select name="" id="select-servicios-extra">
+        divTipo.innerHTML=`<legend> Sevicios extra</legend>`
+        selectCamas.innerHTML=<select name="Servicios Extra" id="select-servicios-extra">
             <option value=" Desayuno">Desayuno</option>
             <option value=" Desayuno y almuerzo">Desayuno y almuerzo</option>
             <option value=" Desayuno, almuerzo y cena">Desayuno, almuerzo y cena</option>
-        </select>`
+        </select>
         
                         break;
 
